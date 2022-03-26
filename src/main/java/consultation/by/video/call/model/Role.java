2c8ca326b71f827cf7f0ff795cb58b3c03fcb406
gabynavarro <alt64b@gmail.com>
2022-03-25 23:56:42 -0300

    estructura security

--- src/main/java/consultation/by/video/call/model/Role.java
+++ src/main/java/consultation/by/video/call/model/Role.java
@@ -1,9 +1,33 @@
 package consultation.by.video.call.model;
 
+import java.sql.Timestamp;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
-//@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
 public class Role {
-
+@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    
+    private long id;
+    @NotNull
+    private String name;
+    private String description;
+    @CreationTimestamp
+    private Timestamp timestamp;
 
 }
--- src/main/java/consultation/by/video/call/model/User.java
+++ src/main/java/consultation/by/video/call/model/User.java
@@ -1,4 +1,46 @@
 package consultation.by.video.call.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
 public class User {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+    @NotNull
+    private String email;
+    @CreationTimestamp
+    private Timestamp timestamp;
+
+    @Column(name = "soft_deleted")
+    private boolean softDeleted = Boolean.FALSE;
+    
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+    @Column(name = "roles_id")
+    private List<Role> roles;
 }
