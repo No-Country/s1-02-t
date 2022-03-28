251019f209bbebf42c58c257b56fa0a5291081ef
Tomas Padilla <padillatomasagustin@gmail.com>
2022-03-28 22:24:34 +0200

    ModeladoPerson - Modelado y repositorio

--- src/main/java/consultation/by/video/call/model/entity/Person.java
+++ src/main/java/consultation/by/video/call/model/entity/Person.java
@@ -1,4 +1,32 @@
 package consultation.by.video.call.model.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String dni;
+    private String imageUrl;
+    private String country;
+    private String province;
+    private String city;
+
 }
