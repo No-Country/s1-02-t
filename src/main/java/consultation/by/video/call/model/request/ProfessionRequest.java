17beed01d18c9e90431f572789d4ad0d035bd541
Tomas Padilla <padillatomasagustin@gmail.com>
2022-03-30 20:15:50 +0200

    Exception - badRequest, illegalArgument, constraintViolation, 500, noSuchElement

--- src/main/java/consultation/by/video/call/model/request/ProfessionRequest.java
+++ src/main/java/consultation/by/video/call/model/request/ProfessionRequest.java
@@ -2,13 +2,19 @@ package consultation.by.video.call.model.request;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProfessionRequest {
+
+    @NotEmpty(message = "Debe ingresar un Titulo.")
     private String title;
+
     private String description;
+
     private String imageUrl;
 }
