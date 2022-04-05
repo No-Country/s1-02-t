8307fc90060688406b6788630f4e6b41135b5d77
Abel3581 <marzoa3581@gmail.com>
2022-04-05 20:31:09 -0300

    ACT

---
+++ src/main/java/consultation/by/video/call/controller/PatientController.java
@@ -0,0 +1,13 @@
+package consultation.by.video.call.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+
+
+}
