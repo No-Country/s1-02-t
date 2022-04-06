8cee8e6b88c147822e728c28e31ce2c60a3d814f
gabynavarro <alt64b@gmail.com>
2022-04-06 11:15:04 -0300

    Entity Patient & Chart

--- src/main/java/consultation/by/video/call/auth/config/seeder/DataBaseSeeders.java
+++ src/main/java/consultation/by/video/call/auth/config/seeder/DataBaseSeeders.java
@@ -21,21 +21,20 @@ import java.util.List;
 public class DataBaseSeeders {
 
     private static final String PASSWORD = "12345678";
-    private static final String HOST_EMAIL = "@test.com";   
+    private static final String HOST_EMAIL = "@test.com";
     private static final String firstNameUser[] = {"Gabriel", "Abel", "Tomas"};
     private static final String firstNamePatient[] = {"Laura", "Raquel", "Roberto"};
     private static final String firstNameProfessional[] = {"Tomas", "Eduardo", "Caro"};
     private static final String lastNameUser[] = {"Navarro", "Acevedo", "Padilla"};
     private static final String lastNameProfessional[] = {"Caruana", "Lopez", "Nose"};
     private static final String lastNamePatient[] = {"Sanchez", "Lepez", "Astudillo"};
-    
+
     @Autowired
     private IUserRepository userRepository;
     @Autowired
     private IRoleRepository roleRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
-
 
     @EventListener
     public void seed(ContextRefreshedEvent event) throws IOException {
@@ -55,81 +54,84 @@ public class DataBaseSeeders {
         createRole(1L, ListRole.USER);
         createRole(2L, ListRole.ADMIN);
         createRole(3L, ListRole.PROFESSIONAL);
-         createRole(4L, ListRole.PATIENT);
+        createRole(4L, ListRole.PATIENT);
     }
 
     private void createUsers() {
-        User user=new User();
+        User user = new User();
         createUsers(ListRole.ADMIN);
         createPatient(ListRole.USER);
         createProfessional(ListRole.PROFESSIONAL);
     }
 
-    private Integer calcAge(){    
-       return (int)(Math.random()*(90-18+1)+18);
+    private Integer calcAge() {
+        return (int) (Math.random() * (90 - 18 + 1) + 18);
     }
-    
-     private String calcDni(){       
-         Integer num=(int) (Math.random() * (99000000-10000000+1)+10000000);
-       return num.toString();
+
+    private String calcDni() {
+        Integer num = (int) (Math.random() * (99000000 - 10000000 + 1) + 10000000);
+        return num.toString();
     }
+
     private void createUsers(ListRole applicationRole) {
 
         for (int index = 0; index < 3; index++) {
-                User user = new User();
-                user.setFirstName(firstNameUser[index]);
-                user.setEmail(applicationRole.getName().toLowerCase() + (index+1) + HOST_EMAIL);
-                user.setPassword(passwordEncoder.encode(PASSWORD));
-                user.setCity("Beltrna ");
-                user.setCountry("Argentina");
-                user.setLastName(lastNameUser[index]);
-                user.setAge(calcAge());               
-                user.setDni(calcDni());
-                user.setProvince("Mendoza");
-                List<Role> roles = new ArrayList<>();
-                roles.add(roleRepository.findByName(applicationRole.getFullRoleName()));
-                user.setRoles(roles);
-                userRepository.save(user);
+            User user = new User();
+            user.setFirstName(firstNameUser[index]);
+            user.setEmail(applicationRole.getName().toLowerCase() + (index + 1) + HOST_EMAIL);
+            user.setPassword(passwordEncoder.encode(PASSWORD));
+            user.setCity("Beltran ");
+            user.setCountry("Argentina");
+            user.setLastName(lastNameUser[index]);
+            user.setAge(calcAge());
+            user.setDni(calcDni());
+            user.setProvince("Mendoza");
+            user.setRoles(createListRole(applicationRole));
+            userRepository.save(user);
 
         }
     }
-     private void createPatient(ListRole applicationRole) {
+
+    private List<Role> createListRole(ListRole applicationRole) {
+        List<Role> roles = new ArrayList<>();
+        roles.add(roleRepository.findByName(applicationRole.getFullRoleName()));
+        return roles;
+    }
+
+    private void createPatient(ListRole applicationRole) {
 
         for (int index = 0; index < 3; index++) {
-                Patient user = new Patient();
-                user.setFirstName(firstNamePatient[index]);
-                user.setEmail(applicationRole.getName().toLowerCase() + (index+1) + HOST_EMAIL);
-                user.setPassword(passwordEncoder.encode(PASSWORD));
-                user.setCity("Beltran ");
-                user.setCountry("Argentina");
-                user.setLastName(lastNamePatient[index]);
-                user.setAge(calcAge());               
-                user.setDni(calcDni());
-                user.setProvince("Mendoza");
-                List<Role> roles = new ArrayList<>();
-                roles.add(roleRepository.findByName(applicationRole.getFullRoleName()));
-                user.setRoles(roles);
-                userRepository.save(user);
+            Patient user = new Patient();
+            user.setFirstName(firstNamePatient[index]);
+            user.setEmail(applicationRole.getName().toLowerCase() + (index + 1) + HOST_EMAIL);
+            user.setPassword(passwordEncoder.encode(PASSWORD));
+            user.setCity("Beltran ");
+            user.setCountry("Argentina");
+            user.setLastName(lastNamePatient[index]);
+            user.setAge(calcAge());
+            user.setDni(calcDni());
+            user.setProvince("Mendoza");
+            user.setRoles(createListRole(applicationRole));
+            userRepository.save(user);
 
         }
     }
-      private void createProfessional(ListRole applicationRole) {
+
+    private void createProfessional(ListRole applicationRole) {
 
         for (int index = 0; index < 3; index++) {
-                Professional user = new Professional();
-                user.setFirstName(firstNameProfessional[index]);
-                user.setEmail(applicationRole.getName().toLowerCase() + (index+1) + HOST_EMAIL);
-                user.setPassword(passwordEncoder.encode(PASSWORD));
-                user.setCity("Beltran ");
-                user.setCountry("Argentina");
-                user.setLastName(lastNameProfessional[index]);
-                 user.setAge(calcAge());               
-                user.setDni(calcDni());
-                user.setProvince("Mendoza");
-                List<Role> roles = new ArrayList<>();
-                roles.add(roleRepository.findByName(applicationRole.getFullRoleName()));
-                user.setRoles(roles);
-                userRepository.save(user);
+            Professional user = new Professional();
+            user.setFirstName(firstNameProfessional[index]);
+            user.setEmail(applicationRole.getName().toLowerCase() + (index + 1) + HOST_EMAIL);
+            user.setPassword(passwordEncoder.encode(PASSWORD));
+            user.setCity("Beltran ");
+            user.setCountry("Argentina");
+            user.setLastName(lastNameProfessional[index]);
+            user.setAge(calcAge());
+            user.setDni(calcDni());
+            user.setProvince("Mendoza");
+            user.setRoles(createListRole(applicationRole));
+            userRepository.save(user);
 
         }
     }
---
+++ src/main/java/consultation/by/video/call/controller/PatientController.java
@@ -0,0 +1,14 @@
+package consultation.by.video.call.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/patient")
+@Api(value = "controller for patient methods", description = "This API for patient methods")
+public class PatientController {
+    
+}
