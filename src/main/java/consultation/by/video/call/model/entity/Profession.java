d2e21e4a9c976e27ab7df25cf0cae139f6466eb3
Gabriel Navarro <85877669+gabynavarro@users.noreply.github.com>
2022-04-07 09:14:33 -0300

    arreglo entidad

--- src/main/java/consultation/by/video/call/model/entity/Profession.java
+++ src/main/java/consultation/by/video/call/model/entity/Profession.java
@@ -3,8 +3,10 @@ package consultation.by.video.call.model.entity;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,16 +22,22 @@ public class Profession {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "profession_id")
     private Long id;
+
     @NotEmpty(message = "Poneme un Titulo amigo!")
     private String title;
+
     @Lob
     @Column
     private String description;
-    private String imageUrl;
-    private boolean deleted = Boolean.FALSE;
 
-    //@ManyToMany(cascade = {CascadeType.ALL}, mappedBy = "professions")
-   // List<Professional> professionals = new ArrayList<>();
+    private String imageUrl;
+
+    private boolean deleted;
+
+    @ManyToMany(cascade = {CascadeType.ALL}, mappedBy = "professions")
+    List<Professional> professionals = new ArrayList<>();
+
+
+
 }
